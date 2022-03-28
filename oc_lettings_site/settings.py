4ef305c9d0e43bf1955804581efe1cc34ae6fcfc
Konrath <36306272+Ikeaven@users.noreply.github.com>
2022-03-28 14:53:40 +0200

    debug false

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://oc-lettings-konrath.herokuapp.com']
